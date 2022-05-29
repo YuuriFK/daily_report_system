96d62d167972c333cf28490b93418ceed621a2b3
yuuri.fujino <yuuri.fujino@gmail.com>
2022-05-29 17:43:12 +0900

    従業員destroyアクションの作成(Chapter6.12)

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -132,4 +132,13 @@ public class EmployeeAction extends ActionBase {
             }
         }
     }
+
+    //destroy
+    public void destroy() throws ServletException, IOException{
+        if(checkToken()) {
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+        }
+    }
 }
